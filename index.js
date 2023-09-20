c215227ef19bb4be3e5435617b7cc6d1af514e2c
Jane Park <jane.sypark@gmail.com>
2023-09-20 08:23:52 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    if (customerName) {
+      customerName = customerName.toUpperCase();
+    }
+  }
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  const leastFavoriteCustomer = 'Joe';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob';
+}
